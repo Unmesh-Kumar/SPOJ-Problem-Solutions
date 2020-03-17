2ade5f788819706272a2f90d76cab45b63e76d60
Unmesh Kumar <ukumar@cs.iitr.ac.in>
2020-03-17 05:46:44 +0530

    TEST - Life, the Universe, and Everything

---
+++ TEST.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{boolean d = true;int a = 0;
+	Scanner in = new Scanner(System.in);
+	while(d == true) {
+	int y = in.nextInt();
+	if (y == 42)   { d =false;} 
+	if (y != 42)  {System.out.println(y);} }
+	
+	
+	}
+}
